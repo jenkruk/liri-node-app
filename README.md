601570647d68e4e620747642780539160e6963db
Jennifer Kruk <jenkruk@gmail.com>
2020-04-08 15:44:15 -0700

    commented the js file and added the readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # liri-node-app
+
+LIRI Bot is a CLI (command line interface) node app that returns data based on user input
+
+To use this app:
+
+1.  Type 'node' into the command line, note that this is considered index 0 on the command line
+2.  Next, or at index 1, type the name of the js file, in this case 'liri.js'
+3.  Next, or at index 2, type one of the following commands:
+    a. concert-this
+    b. spotify-this-song
+    c. movie-this
+    d. do-what-it-says
+
+4. Lastly, at index 3, type either the name of an artist or band, song or movie accordingly.  If you typed
+'do-what-it-says' on index 2, you will just hit enter without any any additional user input.
+
+Pulling in data from the Spotify API, the BandsInTown API and the OMDB API, LiriBot will then bring up the relevant information based on your query and show that data not only in the 
+terminal console, but also in a file that it will generate called log.txt.
+
+* Please note: If you clone this repo for your own use, you will need to create your own .env file
+and then install it as a dependency by including the following line in your .js file: var dotenv = require("dotenv")
+
+Add the following to your .env file, replacing the values with your own spotify API ID and secret (no quotes) once you have them:
+
+# Spotify API keys
+SPOTIFY_ID=your-spotify-id
+SPOTIFY_SECRET=your-spotify-secret
+
+This file will be used by the dotenv package to set what are known as environment variables to the global process.env object in node. These are values that are meant to be specific to the computer that node is running on, and since we are gitignoring this file, they won't be pushed to github — keeping your API key information private.
--- liri.js
+++ liri.js
@@ -1,30 +1,32 @@
 
-// At the top of the liri.js file, add code to read and set any environment variables with the dotenv package:
+// At the top of the liri.js file, add code to read and set any environment variables with the dotenv package
 require("dotenv").config();
 
-//Add the code required to import the keys.js file and store it in a variable.
+// Add the code required to import the keys.js file and store it in a variable.
 var keys = require("./keys.js");
 
-var axios = require("axios");
+// With Axios, liriBot will import data from the bands-in-town api and the omdb api
+var axios = require("axios"); 
+
 var Spotify = require("node-spotify-api");
+
+// Moment will be used to make the date of the concert (data returned from bands-in-town) look better
 var moment = require("moment");
+// Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env
 var dotenv = require("dotenv");
+// The fs module provides an API for interacting with the file system 
 var fs = require("fs");
 
 // Variables for the arguments to be entered by the user
 var request = process.argv[2];
-// console.log("request: " + request);
-// Using the slice method, create a variable for the userInput to handle more than one word userInputs
+
+// Using the slice method, create a variable for the userInput to handle entries containing more than one word 
 var userInput = process.argv.slice(3).join(" ");
-// console.log("userInput: " + userInput);
 
-//  Make it so liri.js can take in one of the following commands:
-//  concert-this
-//  spotify-this-song
-//  movie-this
-//  do-what-it-says
+//  Make it so liri.js can take in one of the following commands: concert-this, spotify-this-song, 
+//  movie-this or do-what-it-says
 
-// Switch Statement to execute the code depending on the request (on index line 2) that the user types in
+// This Switch Statement executes the code depending on the request (on index line 2) that the user enters
 function liriBot(request, userInput) {
   switch (request) {
     case "concert-this":
@@ -40,46 +42,46 @@ function liriBot(request, userInput) {
     break;
 
     case "do-what-it-says":
+    // This function (see line 154) will reference the random.txt doc and follow the command within
       getRandom();
     break;
 
-    // Return a default message if the request line (index 2) is left blank
+    // Returns a default message if the request line (index 2) is left blank
     default:
       console.log("Please enter a request such as 'concert-this', 'spotify-this-song', or 'movie-this'");
   };
 };
 
-//function to search the Spotify API
+//----------------------- FUNCTION TO SEARCH THE SPOTIFY API ----------------------------
 function getSpotify(songName) {
 
   //Variable for the Key & Secret for Spotify
   var spotify = new Spotify(keys.spotify);
 
+    // If the user does not enter a song name or the song name is not found use "The Sign"
     if(!songName) {
       songName = "The Sign";
     };
-    // console.log("Songname if none given: " + songName);
 
   spotify.search({type: 'track', query: songName}, function (err, data) {
     if(err) {
       return console.log('Error occurred: ' + err);
     };
     
-    //console.log("Data for searched song: " + data.tracks.items[0]);
     // adding a line break for clarity of when search results begin
     console.log("\r\n" + "===============================" + "\r\n");
-    //return Artist(s)
+    // returns Artist(s)
     console.log("Artist(s) Name: " + data.tracks.items[0].album.artists[0].name + "\r\n");
-    //return the Song's name
+    // returns the Song's name
     console.log("Song Name: " + data.tracks.items[0].name + "\r\n");
-    //return a preview link of the song from Spotify
+    // returns a preview link of the song from Spotify
     console.log("Song Preview Link: " + data.tracks.items[0].href + "\r\n");
-    //return the Album that the song is from
+    // returns the name of the Album that the song is from
     console.log("Album: " + data.tracks.items[0].album.name + "\r\n");
     // adding a line break for clarity of when search results end
     console.log("===============================" + "\r\n");
 
-    //Append text into log.txt file 
+    // Appends text into log.txt file 
     var logSong = "\r\n" + "===== Begin spotify-this-song log entry =====" + "\r\n" + "\nArtist: " + data.tracks.items[0].album.artists[0].name + "\r\n" + "\nSong Name: " + data.tracks.items[0].name + "\r\n" +  "\nSong Preview Link: " + data.tracks.items[0].href + "\r\n" +  "\nAlbum: " + data.tracks.items[0].album.name + "\r\n" + "\n===== End spotify-this-song log entry =====" + "\r\n";
 
     fs.appendFile("log.txt", logSong, function(err) {
@@ -89,7 +91,7 @@ function getSpotify(songName) {
   });
 };
 
-//----------------------- FUNCTION TO SEARCH BANDS IN TOWN API ----------------------------
+//----------------------- FUNCTION TO SEARCH BANDS IN TOWN API VIA AXIOS ----------------------------
 
 function getBandsInTown(artist) {
 
@@ -115,32 +117,30 @@ function getBandsInTown(artist) {
     });
 };
 
-// Function to search OMDB API
+//----------------------- FUNCTION TO SEARCH THE OMDB API VIA AXIOS ----------------------------
+
 function getOMDB(movie) {
-  // console.log("Movie: " + movie);
   //If the user doesn't type a movie in, the program will output data for the movie "Mr. Nobody"
   if (!movie) {
     movie = "Mr. Nobody";
   }
   var movieQueryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=shor&apikey=trilogy";
-  //console.log(movieQueryURL);
 
   axios.request(movieQueryUrl).then(
     function (response) {
-      //console.log(response.data);
       // adding a line break for clarity of when search results begin
       console.log("\r\n" + "===============================" + "\r\n");
-      console.log("* Title: " + response.data.Title + "\r\n");
-      console.log("* Year Released: " + response.data.Year + "\r\n");
-      console.log("* IMDB Rating: " + response.data.imdbRating + "\r\n");
-      console.log("* Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\r\n");
-      console.log("* Country Where Produced: " + response.data.Country + "\r\n");
-      console.log("* Language: " + response.data.Language + "\r\n");
-      console.log("* Plot: " + response.data.Plot + "\r\n");
-      console.log("* Actors: " + response.data.Actors + "\r\n");
+      console.log("Title: " + response.data.Title + "\r\n");
+      console.log("Year Released: " + response.data.Year + "\r\n");
+      console.log("IMDB Rating: " + response.data.imdbRating + "\r\n");
+      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\r\n");
+      console.log("Country Where Produced: " + response.data.Country + "\r\n");
+      console.log("Language: " + response.data.Language + "\r\n");
+      console.log("Plot: " + response.data.Plot + "\r\n");
+      console.log("Actors: " + response.data.Actors + "\r\n");
       console.log("===============================" + "\r\n");
 
-      //logResults(response);
+      // logResults(response);
       var logMovie = "\r\n" + "========== Begin movie-this log entry =======" + "\r\n" + "\nMovie title: " + response.data.Title + "\nYear released: " + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating + "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\nCountry Where Produced: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors + "\r\n" + "\n========== End movie-this log entry =======" + "\r\n";
 
       fs.appendFile("log.txt", logMovie, function (err) {
@@ -149,8 +149,7 @@ function getOMDB(movie) {
 });
 };
 
-//Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call on of LIRI's commands
-//Function Random
+// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands
 
 function getRandom() {
   fs.readFile("random.txt", "utf8", function (error, data) {
